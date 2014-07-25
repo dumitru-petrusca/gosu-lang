2ffe91dd11bd99eec6ed43b4d90dd40aa85a5e52
Dumitru Petrusca <dpetrusca@guidewire.com>
2014-07-25 11:23:01 -0700

    Compile the Gosu classes from the command line.

--- gosu-core-api/src/main/java/gw/lang/gosuc/simple/SoutCompilerDriver.java
+++ gosu-core-api/src/main/java/gw/lang/gosuc/simple/SoutCompilerDriver.java
@@ -3,10 +3,21 @@ package gw.lang.gosuc.simple;
 import java.io.File;
 
 public class SoutCompilerDriver implements ICompilerDriver {
+  private int nErrors;
 
   @Override
   public void sendCompileIssue(File file, int category, long offset, long line, long column, String message) {
-    System.out.println((category == WARNING ? "Warning: " : "Error: ") + message);
+    String text = "";
+    if (category == WARNING) {
+      text = "Warning: ";
+    } else if (category == ERROR) {
+      text = "Error: ";
+      nErrors++;
+    }
+    System.out.println(text + message);
   }
 
+  public boolean hasErrors() {
+    return nErrors > 0;
+  }
 }
--- gosu-maven-plugin/src/main/java/gw/internal/gosu/maven/CompileMojo.java
+++ gosu-maven-plugin/src/main/java/gw/internal/gosu/maven/CompileMojo.java
@@ -5,7 +5,6 @@
 package gw.internal.gosu.maven;
 
 import com.google.common.collect.Lists;
-import org.apache.maven.model.Resource;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -27,15 +26,6 @@ public class CompileMojo extends AbstractCompileMojo {
   @Parameter(property = "project.build.outputDirectory")
   private File output;
 
-  @Parameter
-  private List<File> sources;
-
-  @Parameter
-  private List<File> classes;
-
-  @Parameter
-  private List<File> jreClasses;
-
   @Override
   protected File getOutputFolder() {
     return output;
@@ -43,29 +33,20 @@ public class CompileMojo extends AbstractCompileMojo {
 
   @Override
   protected List<File> getSources() {
-    if (sources != null) {
-      return sources;
-    }
     List<File> sources = Lists.newArrayList();
     for (String source : mavenProject.getCompileSourceRoots()) {
       sources.add(new File(source));
-    }
-    for (Resource res : mavenProject.getResources()) {
-      sources.add(new File(res.getDirectory()));
     }
     return sources;
   }
 
   @Override
   protected List<File> getClassPath() {
-    if (classes != null) {
-      return classes;
-    }
     File output = new File(mavenProject.getBuild().getOutputDirectory());
     return Collections.singletonList(output);
   }
 
-  protected List<File> getJreClassPath() {
-    return jreClasses != null ? jreClasses : Collections.<File>emptyList();
+  protected List<String> getJreClassPath() {
+    return Collections.emptyList();
   }
 }
