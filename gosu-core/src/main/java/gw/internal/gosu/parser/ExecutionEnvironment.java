f1d5b4a0d5b2572fc8a40f2d30ea6a94f9f0f8e8
Luca Boasso <lboasso@guidewire.com>
2014-11-05 16:25:16 -0800

    Fix "include module" issues
    ensure a source root corresponding with an "include module" is wrapped in a AdditionalDirectory so
    gosu sources residing in the root load from the plugin container (since the source root for the
    include module is not for some reason (?) designed to be part of the classpath)

--- gosu-core/src/main/java/gw/internal/gosu/parser/ExecutionEnvironment.java
+++ gosu-core/src/main/java/gw/internal/gosu/parser/ExecutionEnvironment.java
@@ -12,6 +12,7 @@ import gw.internal.gosu.module.DefaultSingleModule;
 import gw.internal.gosu.module.GlobalModule;
 import gw.internal.gosu.module.JreModule;
 import gw.internal.gosu.module.Module;
+import gw.fs.AdditionalDirectory;
 import gw.lang.cli.SystemExitIgnoredException;
 import gw.lang.gosuc.GosucModule;
 import gw.lang.gosuc.GosucProject;
@@ -163,7 +164,18 @@ public class ExecutionEnvironment implements IExecutionEnvironment
       List<IDirectory> allRoots = new ArrayList<IDirectory>();
       for( GosuPathEntry pathEntry : pathEntries )
       {
-        allRoots.add(pathEntry.getRoot());
+        IDirectory root = pathEntry.getRoot();
+        allRoots.add(root);
+        for( IDirectory dir: pathEntry.getSources() ) {
+          IDirectory srcDir;
+          if( root.isAdditional() ) {
+            srcDir = new AdditionalDirectory( dir );
+          }
+          else {
+            srcDir = dir;
+          }
+          allSources.add( srcDir );
+        }
         allSources.addAll(pathEntry.getSources());
       }
       singleModule.configurePaths(createDefaultClassPath(), allSources);
